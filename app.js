06cc432d42697242cc28581a3f222b22be809bdd
Facundo Loberse <facundosa123@gmail.com>
2019-07-06 20:41:58 -0300

    PegSolitaire v1

---
+++ app.js
@@ -0,0 +1,31 @@
+//Creamos el board inicial
+const initialState = [
+    [undefined, undefined, 1, 1, 1, undefined, undefined],
+    [undefined, undefined, 1, 1, 1, undefined, undefined],
+    [1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 0, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1],
+    [undefined, undefined, 1, 1, 1, undefined, undefined],
+    [undefined, undefined, 1, 1, 1, undefined, undefined]
+];
+
+let dynamicBoard = '<ul>';
+// Creamos elemento ul con elementos li dentro y botones
+for (let i = 0; i < initialState.length; i++) {
+    dynamicBoard += '<li>';
+
+    for (let j = 0; j < initialState[i].length; j++) {
+       dynamicBoard += '<button class= "ball-place"></button>';
+
+    }
+    dynamicBoard +='</li>';
+}
+dynamicBoard += '</ul>';
+
+window.onload = function (){
+    const boardEelement = document.getElementById('board');
+    //Asignamos DynamicBoard
+    boardEelement.innerHTML =  dynamicBoard;
+
+}
+
